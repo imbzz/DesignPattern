194a30bd3f64612e99bdb184e325fee5d536a5b3
imbzz <1756612361@qq.com>
2023-12-19 00:13:53 +0800

    初始化依赖配置

---
+++ src/test/java/com/example/designmodel/DesignModelLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.designmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignModelLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
